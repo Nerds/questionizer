434a642f36afaf6d00f23b39c274e47a1786732e
Jan Kus <jan@railslove.com>
2012-05-01 18:22:52 +0200

    forgot to change omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '4c2c862ba92d48bebff0', 'deefd5fd1f93f95c9941007550ddd1d0557db5f8', :scope => "repo"
+  provider :github, NerdpursuitFrontend::Application.config.github_key, NerdpursuitFrontend::Application.config.github_secret, :scope => "repo"
 end
